d53bd24c34d9e7d86a4e9d01683781bd108ae90a
wengfuyi <214952026@qq.com>
2019-10-08 14:17:12 +0800

    超时测试

---
+++ Chapter5/src/main/java/com/course/testng/TimeOutTest.java
@@ -0,0 +1,16 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+public class TimeOutTest {
+    @Test(timeOut = 3000)
+    public void timeOutSuccess() throws InterruptedException {
+        Thread.sleep(2000);
+    }
+
+    @Test(timeOut = 2000)
+    public void timeOutFail() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
+}
