8d708cb6c5073c2df38c59c6e6e2b531154daded
wengfuyi <214952026@qq.com>
2019-10-14 23:30:06 +0800

    swagger-ui 创建接口文档

--- Chapter10/src/main/java/com/Application.java
+++ Chapter10/src/main/java/com/Application.java
@@ -2,9 +2,11 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 //@ComponentScan("hello")
+@EnableSwagger2
 public class Application{
 
     public static void main(String[] args) {
--- Chapter10/src/main/java/com/course/server/MyGetMethod.java
+++ Chapter10/src/main/java/com/course/server/MyGetMethod.java
@@ -1,6 +1,8 @@
 package com.course.server;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
@@ -13,8 +15,11 @@ import java.util.Map;
 import java.util.Objects;
 
 @RestController
+// swagger 在类名上的注释
+@Api(value = "/", description = "这是我全部的 GET 方法")
 public class MyGetMethod {
     @RequestMapping(path = {"/getcookie"}, method = RequestMethod.GET)
+    @ApiOperation(value = "通过此方法获取 cookie", httpMethod = "GET")
     public String getCookie(HttpServletResponse response){
 //        HTTPServletRequest 装请求信息的类
 //        HTTPServletResponse 装响应信息的类
@@ -28,6 +33,8 @@ public class MyGetMethod {
      */
 
     @RequestMapping(path = {"/get/with/cookie"})
+//    Swagger 对方法的描述，需要添加 http 方法
+    @ApiOperation(value = "需携带 cookie 才会返回成功", httpMethod = "GET")
     public String getWithCookie(HttpServletRequest request){
         Cookie[] cookies = request.getCookies();
         if (Objects.isNull(cookies)){
@@ -49,6 +56,7 @@ public class MyGetMethod {
      */
 
     @RequestMapping(path = {"/get/with/param"})
+    @ApiOperation(value = "携带参数的 Get 方法一", httpMethod = "GET")
     public Map<String, Integer> getWithParam(@RequestParam Integer start,
                                              @RequestParam Integer end){
         Map<String, Integer> myList = new HashMap<>();
@@ -66,6 +74,7 @@ public class MyGetMethod {
      * url:ip:port/get/with/param/10/20
      */
     @RequestMapping(path = {"/get/with/param/{start}/{end}"})
+    @ApiOperation(value = "携带参数的 GET 方法二", httpMethod = "GET")
     public Map getWithPathParam(@PathVariable Integer start,
                                 @PathVariable Integer end){
         Map<String, Integer> myList = new HashMap<>();
